7b5f442fe8665c8b40885ac330c6ac3becab74bf
WesTheHuman2000 <89660042+WesTheHuman2000@users.noreply.github.com>
2022-10-22 18:06:46 -0400

    put a little bit in for the get movie test

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,11 @@
+from app import app
+from src.repositories.movie_repository import get_movie_repository
 # TODO: Feature 1
+
+movie_repository = get_movie_repository()
+
+def test_list_all_movies():
+    test_app = app.test_client()
+    response = test_app.get('/movies')
+  #  movie_repository.create_movie(self, Batman, Bob, 5) #
+    assert b'Movie' in response.data
